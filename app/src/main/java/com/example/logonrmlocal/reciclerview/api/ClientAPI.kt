ce73b5651ed020d407f8e7b13f7ad1fab579ffc8
LeAlbus <pedrolopes.dev@gmail.com>
2019-01-30 20:49:37 -0200

    Api connector base created

--- app/src/main/java/com/example/logonrmlocal/reciclerview/api/ClientAPI.kt
+++ app/src/main/java/com/example/logonrmlocal/reciclerview/api/ClientAPI.kt
@@ -1,9 +1,16 @@
 package com.example.logonrmlocal.reciclerview.api
 
+import android.content.Context
+import android.util.Log
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import com.squareup.picasso.OkHttp3Downloader
+import com.squareup.picasso.Picasso
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 
 class ClientApi<T> {
@@ -20,7 +27,39 @@ class ClientApi<T> {
     }
 }
 
+//Internal singleton (access from this file)
+private var picasso: Picasso? = null
+
+fun getPicassoAuth(context: Context): Picasso{
+
+    if(picasso == null){
+        picasso = Picasso
+                .Builder(context)
+                .downloader(OkHttp3Downloader(getOkhttpClientAuth().build())
+    }
+
+    return picasso!!
+}
+
 fun getOkhttpClientAuth(): OkHttpClient.Builder{
 
     return OkHttpClient.Builder()
+            .addInterceptor(AuthInterceptor())
+            .connectTimeout(30000, TimeUnit.MILLISECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+}
+
+class AuthInterceptor: Interceptor{
+    override fun intercept(chain: Interceptor.Chain?): Response {
+        val requestBuilder = chain!!.request().newBuilder()
+        requestBuilder.addHeader("Authorization", "Basic cG9rZWFwaTpwb2tlbW9u")
+        val request = requestBuilder.build()
+        val response = chain.proceed(request)
+        if (response.code() == 401) {
+            Log.e("MEUAPP", "Error API KEY")
+        }
+        return response
+    }
+
 }
