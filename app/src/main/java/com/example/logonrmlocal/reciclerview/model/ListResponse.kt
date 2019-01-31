abd249df5f205c6f05fee74e5ddae74de84f8074
LeAlbus <pedrolopes.dev@gmail.com>
2019-01-30 22:54:36 -0200

    Fixed

--- app/src/main/java/com/example/logonrmlocal/reciclerview/model/ListResponse.kt
+++ app/src/main/java/com/example/logonrmlocal/reciclerview/model/ListResponse.kt
@@ -2,7 +2,7 @@ package com.example.logonrmlocal.reciclerview.model
 
 import com.google.gson.annotations.SerializedName
 
-data class ListResponse(val pokeList: List<Pokemon>)
+data class ListResponse(@SerializedName("content") val pokeList: List<Pokemon>)
 
 data class Pokemon(
         @SerializedName("number") val pokedexNum: String,
